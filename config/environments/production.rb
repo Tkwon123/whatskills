e10d6a63a7c8e55b33da22d454c3907f1b605709
Tim Won <tkw21189@gmail.com>
2015-06-09 08:37:48 -0500

    fixing the prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['whatskills'],
-    :access_key_id => ENV['AKIAING2EWWTQ4DF5BWA'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
-
 end
